3224233e06fbf4ff8c6af46554609e3380b16a28
black_nuar <hasaevsuliman6@gmail.com>
2023-02-26 11:34:53 +0300

    Lecture two introduction ro programmin languages

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void fill_array(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void print_array(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int index_off(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+fill_array(array);
+print_array(array);
+
+System.Console.WriteLine();
+int pos = index_off(array, 4);
+System.Console.WriteLine(pos);
